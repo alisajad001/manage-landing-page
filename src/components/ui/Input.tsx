dbc99525361fadd3a8a53ca45694ad55b766cf98
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2025-01-16 18:10:28 +0430

    Trim whitespace from className in Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -3,7 +3,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 export const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
   return (
     <input
-      className={`rounded-full py-3 px-6 w-full ${className}`}
+      className={`rounded-full py-3 px-6 w-full ${className}`.trim()}
       {...props}
     />
   );
